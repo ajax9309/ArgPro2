8fd02cf2a37431099414eedc6add78c0059e5778
ajax9309 <alexzomboe@gmail.com>
2022-08-21 00:48:13 -0300

    netbeans

---
+++ ArgPro2/Back/src/main/java/com/myportfolio/ajax9309/Ajax9309Application.java
@@ -0,0 +1,13 @@
+package com.myportfolio.ajax9309;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ajax9309Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ajax9309Application.class, args);
+	}
+
+}
---
+++ ArgPro2/Back/src/test/java/com/myportfolio/ajax9309/Ajax9309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.ajax9309;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ajax9309ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
